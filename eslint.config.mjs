63d092edb2443a27250a25cc44d6cbabe7bed870
Ethan Price <ethanprice082@gmail.com>
2025-01-11 14:19:26 -0500

    changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,8 +7,8 @@ import pluginReact from "eslint-plugin-react";
 export default [
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   { languageOptions: { globals: globals.browser } },
-  { rules: { "react/react-in-jsx-scope": "off" } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  { rules: { "react/react-in-jsx-scope": "off" } },
 ];
